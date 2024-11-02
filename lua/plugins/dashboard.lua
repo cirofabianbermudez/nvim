2f8f55fc9ef3994df16223f3c0bca1aef15f06e2
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-11-02 16:05:11 +0100

    feat: add sessions
    add sessions to dashboard
    add sessions.lua

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -26,23 +26,34 @@ return { -- DONE
       ]]
 
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    logo = string.rep("\n", 1) .. logo .. ""
 
     local opts = {
       theme = "doom",
       hide = {
         statusline = false,
+        tabline = false,
+        winbar = false,
       },
       config = {
         header = vim.split(logo, "\n"),
         center = {
             { action = "enew | startinsert", desc = " New File", key = "n"},
+            { action = "bdelete | so session.vim", desc = " Load Session", key = "s"},
             { action = "Telescope find_files ", desc = " Find Files", key = "f"},
             { action = "Telescope colorscheme ", desc = " Colorscheme", key = "c"},
             { action = "Telescope oldfiles ", desc = " Recent Files", key = "r"},
             { action = "Lazy", desc = " Lazy", key = "l"},
             { action = "q", desc = " Quit", key = "q"},
         },
+        -- footer = {},
+        footer = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          local msg_str = "Startuptime: " .. ms .. " ms"
+          local msg_plugs =  "Plugins: " .. stats.loaded .. " loaded / " .. stats.count .. " installed" 
+          return { msg_str, msg_plugs }
+        end,
       },
     }
     return opts
