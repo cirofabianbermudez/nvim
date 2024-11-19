13af318946f2d4e60631786cd539b727e6114c6f
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-11-19 17:50:38 +0100

    feat: add diffview.lua to the must plugins

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,11 @@
+return { -- DONE
+	enabled = true,
+  cond = true,
+	"sindrets/diffview.nvim",
+	event = "VeryLazy",
+	opts = {
+	},
+	config = function(_, opts)
+		require("diffview").setup(opts)
+	end,
+}
