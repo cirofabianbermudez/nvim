a88baf9a3c68bfe2a245e54949e3aa2962ac718f
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-12-11 09:46:19 +0100

    feat: lua comment generator working

--- doc/verdi_docs.md
+++ doc/verdi_docs.md
@@ -14,3 +14,48 @@ Verdi shortcuts
 | Shortcut | Description            | Manual                 |
 | -------- | ---------------------- | ---------------------- |
 | `G`      | Add signal to waveform | **Signal/Get Signals** |
+
+
+## Dump signals
+
+For more information read the `vcs_ucli_ug.pdf` page 106 
+
+**3 Commands -> Signal Value and Memory Dump Specification Commands -> dump**
+
+```plain
+fsdbDumpfile "novas.fsdb"
+fsdbDumpvars 0 top
+```
+
+```plain
+dump [-file <filename>] [-type FSDB|EVCD|VPD] [-locking]
+
+dump -add <list_of_nids> [-fid <fid>] [-depth <levels>]
+[-aggregates] [-ports|-in|-out|-inout] [-filter=<filter string>] [-msv
+on|off] [-i<N>|-iall] [-isub][-v<N>|-vall] [-va|-vai|-vav]
+
+dump -close [<file_ID>]
+dump -flush <fid> [-fid <fid>]
+dump -autoflush <on | off> [-fid <fid>]
+dump -interval <seconds> [-fid <fid>]
+dump -interval_simTime <time> [-fid <fid>]
+dump -deltaCycle <on | off> [-fid <fid>]
+dump -switch [<newName>] [-fid <fid>]
+dump -forceEvent <on | off> [-fid <fid>]
+dump -filter [=<filter list>] [-fid <fid>]
+dump -showfilter [-fid <fid>]
+dump -power <on | off> [-fid <fid>]
+dump -powerstate <on | off> [-fid <fid>]
+dump -suppress_file <file_name>
+dump -suppress_instance <list_of_instances>
+dump -enable [-fid <fid>]
+dump -disable [-fid <fid>]
+dump -glitch <on|off> [-fid <fid>]
+dump -opened
+dump -msv[on|off]
+```
+
+
+dump -file novas.fsdb -type FSDB -msv on
+dump -add top -depth 0 -fid FSDB0
+
--- tests/main.lua
+++ tests/main.lua
@@ -1,68 +1,66 @@
-function gen_comment(comment, separator, totalLength, commentChar)
+function gen_comment(config)
 
-  local padding = totalLength - #comment - 4
+  -- Set fefault values if not provided in the config table
+  local comment     = config.comment or "COMMENT"
+  local separator   = config.separator or "="
+  local totalLength = config.totalLength or 80
+  local commentChar = config.commentChar or "#"
+  local innerSpace  = config.innerSpace  or 1
+
+  -- Calculate padding
+  local padding = totalLength - #comment - #commentChar - innerSpace*2 - 1
   local leftPadding = math.floor(padding / 2)
   local rightPadding = padding - leftPadding
 
-  local formatted = commentChar .. " " ..  string.rep(separator, leftPadding) .. 
-                    " " .. comment .. " " .. 
+  -- Generate formatted comment
+  local formatted = commentChar .. " " ..  
+                    string.rep(separator, leftPadding) .. 
+                    string.rep(" ",innerSpace) ..
+                    comment ..
+                    string.rep(" ",innerSpace) ..
                     string.rep(separator, rightPadding)
   
   return formatted
 end
 
-function validate_arguments()
-  if #arg > 4 then
-    error("Too many argument provided.")
-  else
-    for i = 1, #arg do
-        print("arg[" .. i .. "]" .. ": " .. arg[i])
-    end
-  end
-end
-
 function parse_arguments()
-  local defaults = {
+  return {
     comment     = arg[1] or "COMMENT",
     separator   = arg[2] or "=",
     totalLength = tonumber(arg[3]) or 80,
-    commentChar = arg[4] or "#"
+    commentChar = arg[4] or "#",
+    innerSpace  = tonumber(arg[5]) or 1
   }
-  return defaults
 end
+
+function validate_arguments()
+  if #arg > 5 then
+    error("Too many argument provided.")
+  end
+end
+
 
 function main()
 
-  -- if #arg > 4 then
-  --   -- Print an error message to stderr
-  --   io.stderr:write("Error: Too many arguments provided.\n")
-  --   io.stderr:write("Usage: lua script.lua [comment] [separator] [totalLength] [commentChar]\n")
-  --
-  --   -- Exit the program with a non-zero status code to indicate an error
-  --   os.exit(1)
-  -- end  
-
-  -- Validate arguments
+  -- Validate command line arguments
   local status, err = pcall(validate_arguments)
 
   if not status then
-    -- Print an error message to stderr
-    -- io.stderr:write("Error: " .. tostring(err) .. "\n")
     io.stderr:write("Error: Too many arguments provided\n")
-    io.stderr:write("Usage: lua main.lua [comment] [separator] [totalLength] [commentChar]\n")
-
-    -- Exit the program with a non-zero status code to indicate an error
+    io.stderr:write("Usage: lua main.lua [comment] [separator] [totalLength] [commentChar] [innerSpace] \n")
     os.exit(1)
-
   end
 
+  -- Parse arguments
   local args = parse_arguments() 
 
-  -- res = gen_comment("TARGETS", "=", 80, "#")
-  res = gen_comment(args.comment, args.separator, args.totalLength, args.commentChar)
-  print(res)
+  -- Generate comment using the table
+  local result = gen_comment(args)
+
+  -- Print the result
+  print(result)
 end
 
-
+-- Call to the main function
 main()
 
