acc1ca85be554fe2973c3fc671eaed0ac78772e4
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2025-05-19 14:39:13 +0200

    feat: update markdown.lua

--- lua/config/markdown.lua
+++ lua/config/markdown.lua
@@ -4,7 +4,7 @@ vim.keymap.set("i", "<C-k>", '[]()<left><esc>"+pF[a', { noremap = true, silent =
 -- vim.keymap.set("n", "<leader>fa", [[:! start chrome "file://%:p"<CR><CR>]], { desc = "Open Markdown Preview", noremap = true, silent = true })
 
 -- Linux
-vim.keymap.set("n", "<leader>fa", [[:! google-chrome "file://%:p"<CR><CR>]], { desc = "Open Markdown Preview", noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>fa", [[:! google-chrome "file://%:p"<CR><CR>]], { desc = "Open Markdown Preview", noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>fc", [[:! code %:p<CR><CR>]], { desc = "Open in VSCode", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>mv", "i```verilog<CR>```<ESC>O", opt)
+-- vim.keymap.set("n", "<leader>fc", [[:! code %:p<CR><CR>]], { desc = "Open in VSCode", noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>mv", "i```verilog<CR>```<ESC>O", opt)
